841d701d4f670a53807051fec6d2560820beb17a
Nic Stelter <nicholas.stelter@gmail.com>
2021-11-23 10:15:34 -0500

    init commit

---
+++ pages/[[...page]].tsx
@@ -0,0 +1,69 @@
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
+import { BuilderComponent, Builder, builder } from '@builder.io/react'
+import DefaultErrorPage from 'next/error'
+import Head from 'next/head'
+import "../components/heading";
+
+const BUILDER_API_KEY = '8364fce7474142998b6dc4fb85086def'
+builder.init(BUILDER_API_KEY)
+
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext<{ page: string[] }>) {
+  const page = await builder.get('page', {
+    userAttributes: {
+      urlPath: '/' + (params?.page?.join('/') || ''),
+    }
+  })
+  .toPromise() || null
+
+  return {
+    props: {
+      page,
+    },
+    revalidate: 5,
+  }
+}
+
+export async function getStaticPaths() {
+  const pages = await builder.getAll('page', {
+    options: { noTargeting: true }
+  })
+
+  return {
+    paths: pages.map((page) => `${page.data?.url}`),
+    fallback: true,
+  }
+}
+
+export default function Page({
+  page,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <h1>Loading...</h1>
+  }
+  const isLive = !Builder.isEditing && !Builder.isPreviewing
+  if (!page && isLive) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+          <meta name="title"></meta>
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      
+      <BuilderComponent model="page" content={page} />
+    </>
+  )
+}
